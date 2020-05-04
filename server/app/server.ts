7a3426fded8deb3485612790d5f21f2c014dbb8b
Mike V <mvondrak@gmail.com>
2020-05-04 01:57:28 -0600

    port from env, procfile, start command

--- server/app/server.ts
+++ server/app/server.ts
@@ -1,14 +1,15 @@
 // equivalent of older: const express = require('express')
 import express from 'express';
+const PORT = process.env.PORT || 3000;
 
 // Create a new express application instance
 const app: express.Application = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello WWWorld!');
+  res.send('Node Server Works');
 });
 
-app.listen(3000, function () {
+app.listen(PORT, function () {
   console.log('Example app listening on port 3000!');
 });
 
