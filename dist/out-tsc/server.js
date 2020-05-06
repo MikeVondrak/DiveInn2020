b88a7a1eff410e1e6907644dd0859913d9a44330
Mike V <mvondrak@gmail.com>
2020-05-05 22:04:32 -0600

    rerun tsc

--- dist/out-tsc/server.js
+++ dist/out-tsc/server.js
@@ -2,29 +2,23 @@
 import express from 'express';
 import compression from 'compression';
 import * as path from 'path';
-
 const _port = process.env.PORT || 3000; // process.env.PORT set by server (e.g. Heroku) when hosted, or use 3000 for local testing
 const _appLocation = '../../dist/dive-inn/'; // output from ng build --prod
-
 // create a new express application instance
-const app: express.Application = express();
-
+const app = express();
 // gzip compression middleware for performance
 app.use(compression());
-
 console.log("Running in: " + __dirname);
-
 // serve static files
 // __dirname = location where node script is currently executing
 // use path.join to resolve relative path ('../') in _appLocation
 // /server/dist/server.js -> ../../dist/dive-inn -> /dist/dive-inn/
 app.get('*.*', express.static(path.join(__dirname, _appLocation)));
-
 // all app paths -> serve default file: www.diveinndenver.com/ -> look for index.html etc
 app.all('*', function (req, res) {
-  res.status(200).sendFile(`/`, {root: _appLocation});
+    res.status(200).sendFile(`/`, { root: _appLocation });
 });
-
 app.listen(_port, function () {
-  console.log("Node Express server for " + app.name + " listening on port: " + _port);
+    console.log("Node Express server for " + app.name + " listening on port: " + _port);
 });
+//# sourceMappingURL=server.js.map
--- server/dist/server.js
+++ server/dist/server.js
@@ -2,18 +2,30 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 // const express = require('express'); // old js way of importing
 var express_1 = __importDefault(require("express"));
 var compression_1 = __importDefault(require("compression"));
+var path = __importStar(require("path"));
 var _port = process.env.PORT || 3000; // process.env.PORT set by server (e.g. Heroku) when hosted, or use 3000 for local testing
-var _appLocation = '../dist/dive-inn'; // output from ng build --prod
+var _appLocation = '../../dist/dive-inn/'; // output from ng build --prod
 // create a new express application instance
 var app = express_1.default();
 // gzip compression middleware for performance
 app.use(compression_1.default());
+console.log("Running in: " + __dirname);
 // serve static files
-app.get('*.*', express_1.default.static(_appLocation, { maxAge: '1y' }));
+// __dirname = location where node script is currently executing
+// use path.join to resolve relative path ('../') in _appLocation
+// /server/dist/server.js -> ../../dist/dive-inn -> /dist/dive-inn/
+app.get('*.*', express_1.default.static(path.join(__dirname, _appLocation)));
 // all app paths -> serve default file: www.diveinndenver.com/ -> look for index.html etc
 app.all('*', function (req, res) {
     res.status(200).sendFile("/", { root: _appLocation });
