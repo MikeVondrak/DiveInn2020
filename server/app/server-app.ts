b7afe2f87651605636afdb98c8b6cb5ea2a1aa0c
Mike V <mvondrak@gmail.com>
2020-05-25 03:45:37 -0600

    make queries using Pool, add rxjs

--- server/app/server-app.ts
+++ server/app/server-app.ts
@@ -1,26 +1,39 @@
 // https://dev.to/aligoren/developing-an-express-application-using-typescript-3b1
 
 import express, { Application, Request, Response, NextFunction, RequestHandler, Router } from 'express';
-import mysql, { ConnectionOptions, ConnectionConfig, Connection, Query, QueryFunction, QueryOptions, MysqlError, FieldInfo, queryCallback } from 'mysql';
+import mysql, { ConnectionOptions, ConnectionConfig, Connection, Query, QueryFunction, QueryOptions, MysqlError, FieldInfo, queryCallback, Pool, PoolConfig } from 'mysql';
 import { TestData, TestJson } from './models/test-data.model';
+import { routes } from './routes';
+import { Observable, Observer } from 'rxjs';
 
 export class ServerApp {
   private app: Application;
+  private pool: Pool;
+  private readonly dbConfig: ConnectionConfig = {
+    host: 'localhost',
+    port: 3306,
+    user: 'DiveMaster',
+    password: 'D1v3M4st3r!!',
+    database: 'dive_inn_test_db',
+  };
 
   constructor(
-    public port: string = '3000',
-    public staticPathList: string[],
+    private angularAppLocation: string = '',
+    private port: string = '3000',
+    private staticPathList: string[],
     private middleWareList: RequestHandler[],
     private controllerList: Router[]
   ) {
     this.app = express(); // create a new express application instance
     this.port = process.env.PORT ? process.env.PORT : this.port; // process.env.PORT set by Heroku
+    this.pool = this.createPool(this.dbConfig);
 
-    // first to match route takes precedence,  static > middleware > controllers
-    // '/' defaults to 
+    // first to match route takes precedence,  static > middleware > controllers  
+    // '/' defaults to index.html from Express settings
     this.useStatic(this.staticPathList);
     this.useMiddleware(this.middleWareList);
     this.useControllers(this.controllerList);
+    this.setCatchAllRoutes();
   }
 
   /**
@@ -54,7 +67,19 @@ export class ServerApp {
    */
   private useControllers(routers: Router[]) {
     routers.forEach(router => {
-      this.app.use('*', router);
+      //this.app.use(routes.api.root, router);
+      this.app.use(routes.api._root, router);
+    });
+  }
+
+  /**
+   * Catch all unhandled routes
+   * @TODO proper 404 etc error handling
+   */
+  private setCatchAllRoutes() {
+    this.app.all(routes.error._404, (req, res) => {      
+      //res.status(200).sendFile('/', {root: angularAppLocation});
+      res.status(404).send('Route Not Found');
     });
   }
 
@@ -68,51 +93,72 @@ export class ServerApp {
     })
   }
 
-  public connectDb() {
-    //var mysql = require('mysql');
-    let connectionConfig: ConnectionConfig = {
-      host: 'localhost',
-      port: 3306,
-      user: 'DiveMaster',
-      password: 'D1v3M4st3r!!',
-      database: 'dive_inn_test_db',
+  private createPool(dbConfig: ConnectionConfig, poolSize: number = 100): Pool {
+    const poolConfig: PoolConfig = {
+      connectionLimit: poolSize,
+      ...dbConfig
     };
-    let connection: Connection = mysql.createConnection(connectionConfig);
-    connection.connect(err => {
-      if (err) {
-        throw err;
-      }
-      console.log('Connected to ' + connection.config.database + ' on ' + connection.config.host + ':' + connection.config.port);
-      
-      this.runSimpleQuery(connection);
-    });
+    return mysql.createPool(poolConfig);
   }
 
+  /**
+   * Query using pool, automatically aquires and releases connection to db
+   * @template T
+   * @param sqlQuery SQL query string
+   * @returns Observable of array of provided type, containing query results
+   */
+  //public poolQuery<T>(sqlQuery: string, callback: queryCallback): void {
+  public poolQuery<T>(sqlQuery: string): Observable<T[]> {
+
+    const queryResult$ = (observer: Observer<T[]>) => {
+      
+      const queryOptions: QueryOptions = {
+        sql: sqlQuery,
+      };
+      
+      const responseCallback: queryCallback = (err: MysqlError | null, rows: T[], fields: FieldInfo[] | undefined) => {
+        if (err) {
+          observer.error(err);
+        } 
+        observer.next(rows);
+        observer.complete();
+      };
+      
+      this.pool.query(queryOptions, responseCallback);
+    };
+
+    return new Observable<T[]>(queryResult$);
+  }
+
+
+  // Not sure we need to explicitly disconnet or do any cleanup?
+  // Theoretically Node/Express "should never stop running"
+  // can attach an event handler to process.on('exit', () => {})
   public disconnectDb(connection: Connection) {
     connection.end();
   }
 
 
-  private runSimpleQuery(connection: Connection): Query  {
-    let queryOptions: QueryOptions = {
-      sql: 'SELECT * FROM test_table',
-    };
-    let testData: TestData[] = [];
-    let testDataCallback: queryCallback = (err: MysqlError | null, rows: TestData[], fields: FieldInfo[] | undefined) => {
-      if (err) {
-        throw err;
-      }
-      testData = rows;
+  // private simpleQueryViaConnection(connection: Connection): Query {
+  //   let queryOptions: QueryOptions = {
+  //     sql: 'SELECT * FROM test_table',
+  //   };
+  //   let testData: TestData[] = [];
+  //   let testDataCallback: queryCallback = (err: MysqlError | null, rows: TestData[], fields: FieldInfo[] | undefined) => {
+  //     if (err) {
+  //       throw err;
+  //     }
+  //     testData = rows;
 
-      console.log('\nData:\n');
-      console.log(JSON.stringify(testData, null, 4));
+  //     console.log('\nData:\n');
+  //     console.log(JSON.stringify(testData, null, 4));
 
-      /** @TODO disconnect when app is destroyed */
-      this.disconnectDb(connection);
-    };
+  //     /** @TODO disconnect when app is destroyed */
+  //     this.disconnectDb(connection);
+  //   };
 
-    console.log('Querying: ' + queryOptions.sql);
-    return connection.query(queryOptions, testDataCallback);
-  }
-    
+  //   console.log('Querying: ' + queryOptions.sql);
+  //   return connection.query(queryOptions, testDataCallback);
+  // }
+
 }
